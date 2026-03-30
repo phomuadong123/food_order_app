e31a90429561195a788a4ad87f5f6068843aafa5
phomuadong123 <tuandahe176514@fpt.edu.vn>
2026-03-30 14:39:08 +0700

    fix bug

--- food_order_app/www/vote.js
+++ food_order_app/www/vote.js
@@ -10,7 +10,12 @@ function confirmCancel() {
     );
 }
 
-window.onload = function() {
-    const newUrl = '/api/method/food_order_app.api.start_vote';
-    window.history.replaceState({}, '', newUrl);
-};
+window.addEventListener('load', function() {
+    console.log("Tất cả tài nguyên đã tải xong!");
+    setTimeout(function() {
+        const newUrl = '/api/method/food_order_app.api.start_vote';
+        window.history.replaceState({ path: newUrl }, '', newUrl);
+        
+        console.log("URL đã được làm sạch thành: " + newUrl);
+    }, 1000); 
+});
